a3908a33e3744b8db0b8f0c2e9a27132315860a8
MadFrey <liurongcheng@redrock.team>
2023-09-13 08:24:40 +0800

    Lab: Multithreading

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -13,6 +13,8 @@ struct entry {
   int value;
   struct entry *next;
 };
+pthread_mutex_t  lock[5];
+
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
@@ -39,7 +41,7 @@ static
 void put(int key, int value)
 {
   int i = key % NBUCKET;
-
+    pthread_mutex_lock(&lock[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -53,19 +55,19 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
+    pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
 get(int key)
 {
   int i = key % NBUCKET;
-
-
+    pthread_mutex_lock(&lock[i]);
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
-
+    pthread_mutex_unlock(&lock[i]);
   return e;
 }
 
@@ -76,7 +78,9 @@ put_thread(void *xa)
   int b = NKEYS/nthread;
 
   for (int i = 0; i < b; i++) {
+
     put(keys[b*n + i], n);
+
   }
 
   return NULL;
@@ -102,7 +106,9 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
-
+    for(int i=0;i<NBUCKET;i++) {
+        pthread_mutex_init(&lock[i],NULL);
+    }
   if (argc < 2) {
     fprintf(stderr, "Usage: %s nthreads\n", argv[0]);
     exit(-1);
